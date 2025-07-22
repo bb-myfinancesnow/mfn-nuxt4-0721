3aebef5912ec4a5e1c300c26fa287dfdb65e46b9
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-21 19:53:05 -0500

    eslint updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
-  devtools: { enabled: true },
-  compatibilityDate: '2025-07-15',
-  typescript: {
-    typeCheck: true
-  },
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs',
-        semi: true,
-        quoteProps: 'as-needed',
-        arrowParens: true,
-        indent: 'tab'
-      }
-    }
-  }
+	modules: ['@nuxt/eslint'],
+	devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	typescript: {
+		typeCheck: true
+	},
+	eslint: {
+		config: {
+			stylistic: {
+				commaDangle: 'never',
+				braceStyle: '1tbs',
+				semi: true,
+				quoteProps: 'as-needed',
+				arrowParens: true,
+				indent: 'tab'
+			}
+		}
+	}
 });
