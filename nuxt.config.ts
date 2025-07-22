2e5beff101a9419cb69d22134e4af496a9d1eca5
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-21 20:24:02 -0500

    install bettersqllite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint', '@nuxt/ui-pro'],
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/image',
+		'@nuxt/ui-pro',
+		'@nuxt/content',
+		'@vueuse/nuxt'
+	],
 
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
