6caffd803b2f17f73ca5aa7637dc4ac7ae403eff
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-25 20:44:21 -0500

    base transform

--- app/pages/dash/tiller/accounts.vue
+++ app/pages/dash/tiller/accounts.vue
@@ -1,9 +1,40 @@
 <script lang="ts" setup>
+import {
+	SearchTillerAccRecordsDocument,
+	SortOrder,
+	type SearchTillerAccRecordsQueryVariables,
+	type SearchTillerAccRecordsQuery
+} from '~/generated/graphql';
 
+const { request } = useGql();
+
+const { data: tillerData, pending } = await useLazyAsyncData(
+	'listtilleraccs',
+	() =>
+		request<
+			SearchTillerAccRecordsQuery,
+			SearchTillerAccRecordsQueryVariables
+		>(SearchTillerAccRecordsDocument, {
+			orderBy: [{ id: SortOrder.Asc }]
+		}),
+	{
+		transform: (input) => {
+			const arrSchema = TillerAccRecordSchema.array();
+			const parsedData = arrSchema.parse(input.tillerAccounts);
+			return parsedData;
+		}
+	}
+);
 </script>
 
 <template>
 	<div>
-		tiller accounts
+		<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
+			<div>
+				data:
+				<pre>{{ tillerData }}</pre>
+			</div>
+			<div>status: {{ String(pending) }}</div>
+		</UPageGrid>
 	</div>
 </template>
--- app/pages/dash/tiller/categories.vue
+++ app/pages/dash/tiller/categories.vue
@@ -1,9 +1,33 @@
 <script lang="ts" setup>
+import {
+	SearchTillerCatRecordsDocument,
+	SortOrder,
+	type SearchTillerCatRecordsQueryVariables,
+	type SearchTillerCatRecordsQuery
+} from '~/generated/graphql';
 
+const { request } = useGql();
+
+const { data: tillerData, pending } = await useLazyAsyncData(
+	'listtillercats',
+	() =>
+		request<
+			SearchTillerCatRecordsQuery,
+			SearchTillerCatRecordsQueryVariables
+		>(SearchTillerCatRecordsDocument, {
+			orderBy: [{ id: SortOrder.Asc }]
+		})
+);
 </script>
 
 <template>
 	<div>
-		tiller categories
+		<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
+			<div>
+				data:
+				<pre>{{ tillerData }}</pre>
+			</div>
+			<div>status: {{ String(pending) }}</div>
+		</UPageGrid>
 	</div>
 </template>
