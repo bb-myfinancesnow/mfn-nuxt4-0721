bfc34db28adefa402fc8f6c844df485c309b3582
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-26 01:16:05 -0500

    first try with hands on table

--- app/pages/dash/reporting/tester.vue
+++ app/pages/dash/reporting/tester.vue
@@ -1,37 +1,15 @@
 <script setup lang="ts">
-// const toast = useToast();
+import { data } from '~/example-data/hands-on-example';
 
-const reportData = useReportData();
-
-const {
-	data: periodData,
-	pending,
-	refresh: refreshPeriodQuery
-} = await reportData.searchPeriods();
+const tableData = computed(() => {
+	return data;
+});
 </script>
 
 <template>
-	<div>
-		<UPageCard title="Period Home">
-			<div class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto">
-				<UTooltip text="Refresh Periods">
-					<UButton
-						icon="i-lucide-rotate-ccw-key"
-						class="rounded-full"
-						size="lg"
-						:loading="pending"
-						@click="refreshPeriodQuery()"
-					/>
-				</UTooltip>
-			</div>
-			<PeriodsFlatTable :is-loading="pending" :period-data="periodData" />
-		</UPageCard>
-		<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
-			<div>
-				data:
-				<pre>{{ periodData }}</pre>
-			</div>
-			<div>status: {{ String(pending) }}</div>
-		</UPageGrid>
+	<div class="w-full">
+		<ClientOnly fallback-tag="span" fallback="Loading...">
+			<HandGridExample :table-data="tableData" />
+		</ClientOnly>
 	</div>
 </template>
