af72283421e71439d916fbcf800858cff2a9682c
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-25 21:18:54 -0500

    pagination

--- app/pages/dash/tiller/accounts.vue
+++ app/pages/dash/tiller/accounts.vue
@@ -6,10 +6,16 @@ import {
 	type SearchTillerAccRecordsQueryVariables,
 	type SearchTillerAccRecordsQuery
 } from '~/generated/graphql';
+import { upperFirst } from 'scule';
+import { getPaginationRowModel } from '@tanstack/vue-table';
 
 const { request } = useGql();
 
-const { data: tillerData, pending } = await useLazyAsyncData(
+const {
+	data: tillerData,
+	pending,
+	refresh: refreshAccQuery
+} = await useLazyAsyncData(
 	'listtilleraccs',
 	() =>
 		request<
@@ -32,6 +38,11 @@ const table = useTemplateRef('table');
 const columnVisibility = ref({
 	createdAt: false,
 	updatedAt: false
+});
+
+const pagination = ref({
+	pageIndex: 0,
+	pageSize: 10
 });
 
 const columns: TableColumn<TTillerAccRecordSchema>[] = [
@@ -73,7 +84,14 @@ const q = ref('');
 
 <template>
 	<div>
-		<UPageCard variant="subtle" :ui="{ container: 'p-0 sm:p-0 gap-y-0', wrapper: 'items-stretch', header: 'p-4 mb-0 border-b border-default' }">
+		<UPageCard
+			variant="subtle"
+			:ui="{
+				container: 'p-0 sm:p-0 gap-y-0',
+				wrapper: 'items-stretch',
+				header: 'p-4 mb-0 border-b border-default'
+			}"
+		>
 			<template #header>
 				<UInput
 					v-model="q"
@@ -85,17 +103,62 @@ const q = ref('');
 			</template>
 
 			<template #body>
-				<div>
-					xxx
+				<div
+					class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto"
+				>
+					<UTooltip text="Refresh Accounts">
+						<UButton
+							icon="i-lucide-rotate-ccw-key"
+							class="rounded-full"
+							size="lg"
+							:loading="pending"
+							@click="refreshAccQuery()"
+						/>
+					</UTooltip>
+					<UDropdownMenu
+						:items="
+							table?.tableApi
+								?.getAllColumns()
+								.filter((column) => column.getCanHide())
+								.map((column) => ({
+									label: upperFirst(column.id),
+									type: 'checkbox' as const,
+									checked: column.getIsVisible(),
+									onUpdateChecked(checked: boolean) {
+										table?.tableApi
+											?.getColumn(column.id)
+											?.toggleVisibility(!!checked);
+									},
+									onSelect(e?: Event) {
+										e?.preventDefault();
+									}
+								}))
+						"
+						:content="{ align: 'end' }"
+					>
+						<UButton
+							label="Columns Select"
+							color="neutral"
+							variant="outline"
+							trailing-icon="i-lucide-chevron-down"
+							class="ml-auto"
+							aria-label="Columns select dropdown"
+							:disabled="pending"
+						/>
+					</UDropdownMenu>
 				</div>
 			</template>
 		</UPageCard>
 		<UTable
 			ref="table"
 			v-model:column-visibility="columnVisibility"
+			v-model:pagination="pagination"
 			:data="tillerData"
 			:loading="pending"
 			:columns="columns"
+			:pagination-options="{
+				getPaginationRowModel: getPaginationRowModel()
+			}"
 			class="w-full"
 			:ui="{
 				base: 'table-fixed border-separate border-spacing-0',
@@ -105,6 +168,23 @@ const q = ref('');
 				td: 'border-b border-default'
 			}"
 		/>
+		<div class="flex justify-center border-t border-default pt-4">
+			<UPagination
+				:default-page="
+					(table?.tableApi?.getState().pagination.pageIndex
+						|| 0) + 1
+				"
+				:items-per-page="
+					table?.tableApi?.getState().pagination.pageSize
+				"
+				:total="
+					table?.tableApi?.getFilteredRowModel().rows.length
+				"
+				@update:page="
+					(p) => table?.tableApi?.setPageIndex(p - 1)
+				"
+			/>
+		</div>
 		<!-- <UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
 			<div>
 				data:
