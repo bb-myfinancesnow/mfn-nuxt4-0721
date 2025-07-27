10c6e0b38b128e1f2e548a848c32273d12042169
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-27 18:30:07 -0500

    example pivot

---
+++ app/components/fusion/ExamplePivot.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { testPivotDataCountryState, type ITestPivotCountryState } from '~/example-data/pivot-example';
+
+interface Props {
+	entryData: TFlatJournalEntryLedgerRecSchema[];
+	isLoading: boolean;
+}
+
+defineProps<Props>();
+
+const testPivotData = ref<ITestPivotCountryState[]>(testPivotDataCountryState);
+
+const dataSourceSettings = {
+	dataSource: testPivotData.value,
+	rows: [{ name: 'Country' }],
+	columns: [{ name: 'Date' }],
+	values: [{ name: 'Amount' }, { name: 'Total', type: 'CalculatedField' }],
+	formatSettings: [{ name: 'Amount', format: 'C1' }],
+	calculatedFieldSettings: [
+		{ name: 'Total', formula: '"Sum(Amount)"+"Sum(Quantity)"' }
+	]
+};
+const showFieldList = true;
+const showGroupingBar = true;
+const allowCalculatedField = true;
+const height = 1000;
+const width = '100%';
+</script>
+
+<template>
+	<div>
+		<ejs-pivotview
+			:height="height"
+			:width="width"
+			:data-source-settings="dataSourceSettings"
+			:show-field-list="showFieldList"
+			:show-grouping-bar="showGroupingBar"
+			:allow-calculated-field="allowCalculatedField"
+		/>
+		<!-- <div class="flex justify-between">
+			<div>
+				entryData:
+				<pre>{{ entryData }}</pre>
+			</div>
+			<div>
+				entryData:
+				<pre>{{ dataSourceSettings.dataSource }}</pre>
+			</div>
+		</div> -->
+	</div>
+</template>
