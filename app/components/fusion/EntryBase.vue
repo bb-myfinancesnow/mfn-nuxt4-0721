36ce2c7fbc2e1c23de5a4e0248517c4e11ac88ad
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-27 23:00:17 -0500

    more setup

--- app/components/fusion/EntryBase.vue
+++ app/components/fusion/EntryBase.vue
@@ -1,15 +1,53 @@
 <script lang="ts" setup>
-import { PivotViewComponent as EjsPivotview, type IDataSet } from '@syncfusion/ej2-vue-pivotview';
-import type { DataSourceSettingsModel } from '@syncfusion/ej2-pivotview/src/model/datasourcesettings-model';
+import {
+	PivotViewComponent as EjsPivotview,
+	type IDataSet,
+	type LoadEventArgs,
+	type ToolbarItems
+} from '@syncfusion/ej2-vue-pivotview';
+import type {
+	DataSourceSettingsModel,
+	FieldOptionsModel
+} from '@syncfusion/ej2-pivotview/src/model/datasourcesettings-model';
 
 interface Props {
 	entryData: TFlatJournalEntryLedgerRecSchema[];
 	isLoading: boolean;
 }
 
+const periodSortOptions = computed(() => {
+	const pers = props.entryData.map(({ periodId, periodLabel }) => ({
+		periodId,
+		periodLabel
+	}));
+
+	const sorted = pers
+		.sort((a, b) => a.periodId - b.periodId)
+		.map(({ periodLabel }) => periodLabel);
+	const deDupe = deDupStringArray(sorted);
+	return deDupe;
+});
 const props = defineProps<Props>();
 
 const pivotDataVals = ref<IPivotJournalEntryData[]>([]);
+
+const pivotFieldMapping: FieldOptionsModel[] = [
+	{
+		name: 'glAccountType',
+		caption: 'Account Type',
+		dataType: 'string'
+	},
+	{
+		name: 'glAccountTypeSortOrder',
+		caption: 'Account Type Order',
+		dataType: 'number'
+	},
+	{
+		name: 'postingDate',
+		caption: 'Posting Date',
+		showSortIcon: false
+	}
+];
 
 const getPivotDataVals = (): IDataSet[] => {
 	const res = formatEntryLedgerPivotData(props.entryData);
@@ -17,21 +55,55 @@ const getPivotDataVals = (): IDataSet[] => {
 	return res;
 };
 
-const dataSourceSettings: DataSourceSettingsModel = {
+const dataSourceSettings = ref<DataSourceSettingsModel>({
 	dataSource: getPivotDataVals(),
 	expandAll: false,
-	rows: [{ name: 'accountClass', caption: 'Class' }, { name: 'glAccountLabel', caption: 'Account' }],
+	enableSorting: true,
+	sortSettings: [
+		{
+			name: 'accountClass',
+			order: 'Ascending',
+			membersOrder: ['Asset', 'Liability', 'Equity', 'Revenue', 'Expense']
+		},
+		{
+			name: 'periodLabel',
+			order: 'Ascending',
+			membersOrder: periodSortOptions.value
+		}
+	],
+	rows: [
+		{ name: 'accountClass', caption: 'Class' },
+		{ name: 'glAccountLabel', caption: 'Account' }
+	],
 	columns: [{ name: 'periodLabel', caption: 'Period' }],
 	values: [{ name: 'entryAmount', caption: 'Amounts' }],
-	formatSettings: [{ name: 'entryAmount', format: 'C0' }],
+	formatSettings: [
+		{ name: 'entryAmount', format: 'C2' },
+		{ name: 'postingDate', type: 'date', format: 'MM/dd/yyyy' }
+	],
+	fieldMapping: pivotFieldMapping,
 	filters: []
-};
+});
 const showFieldList = true;
 const showGroupingBar = true;
 const allowCalculatedField = true;
-const height = 1000;
-const width = '100%';
+const height = ref(1000);
+const width = ref('100%');
 
+const allowExcelExport = true;
+const allowPdfExport = true;
+const showToolbar = true;
+const toolbar: ToolbarItems[] = [
+	'Export',
+	'SubTotal',
+	'GrandTotal',
+	'ConditionalFormatting',
+	'NumberFormatting',
+	'FieldList'
+];
+const allowConditionalFormatting = true;
+const allowNumberFormatting = true;
+// const showToolbar
 const setPivotDataVals = () => {
 	const res = formatEntryLedgerPivotData(props.entryData);
 	pivotDataVals.value = res;
@@ -39,17 +111,72 @@ const setPivotDataVals = () => {
 
 const refreshDataSource = () => {
 	setPivotDataVals();
-	pivotviewbase.value.refresh();
+	// pivotviewbase.value.refresh();
 };
 
-const pivotviewbase = ref();
+const pivotviewbase = useTemplateRef<EjsPivotview>('pivotviewbase');
+
+const onDataBound = () => {
+	console.log('data bound');
+	// const pivotObj = document.getElementById('pivotviewbase').ej2_instances[0];
+	const pivotObj = pivotviewbase.value?.ej2Instances;
+
+	if (pivotObj) {
+		console.log('has obj');
+	}
+};
+
+const load = (args: LoadEventArgs) => {
+	const dsSetting: DataSourceSettingsModel = {
+		dataSource: getPivotDataVals(),
+		expandAll: false,
+		enableSorting: true,
+		sortSettings: [
+			{
+				name: 'accountClass',
+				order: 'Ascending',
+				membersOrder: [
+					'Asset',
+					'Liability',
+					'Equity',
+					'Revenue',
+					'Expense'
+				]
+			},
+			{
+				name: 'periodLabel',
+				order: 'Ascending',
+				membersOrder: periodSortOptions.value
+			}
+		],
+		rows: [
+			{ name: 'accountClass', caption: 'Class' },
+			{ name: 'glAccountLabel', caption: 'Account' }
+		],
+		columns: [{ name: 'periodLabel', caption: 'Period' }],
+		values: [{ name: 'entryAmount', caption: 'Amounts' }],
+		formatSettings: [
+			{ name: 'entryAmount', format: 'C2' },
+			{ name: 'postingDate', type: 'date', format: 'MM/dd/yyyy' }
+		],
+		fieldMapping: pivotFieldMapping,
+		filters: []
+	};
+
+	dataSourceSettings.value = dsSetting;
+
+	args.dataSourceSettings = dsSetting;
+};
 </script>
 
 <template>
 	<div>
 		<div class="flex-1">
-			<UButton label="Refresh" @click="refreshDataSource" />
-			<ejs-pivotview
+			<UButton
+				label="Refresh"
+				@click="refreshDataSource"
+			/>
+			<EjsPivotview
 				id="pivotviewbase"
 				ref="pivotviewbase"
 				:height="height"
@@ -58,6 +185,14 @@ const pivotviewbase = ref();
 				:show-field-list="showFieldList"
 				:show-grouping-bar="showGroupingBar"
 				:allow-calculated-field="allowCalculatedField"
+				:allow-excel-export="allowExcelExport"
+				:allow-pdf-export="allowPdfExport"
+				:allow-number-formatting="allowNumberFormatting"
+				:allow-conditional-formatting="allowConditionalFormatting"
+				:show-toolbar="showToolbar"
+				:toolbar="toolbar"
+				:data-bound="onDataBound"
+				:load="load"
 			/>
 		</div>
 
@@ -72,10 +207,10 @@ const pivotviewbase = ref();
 				dataSourceSettings:
 				<pre>{{ dataSourceSettings }}</pre>
 			</div>
-			<!-- <div>
-				dataSourceSettings:
-				<pre>{{ pivotviewbase. }}</pre>
-			</div> -->
+			<div>
+				periodSortOptions:
+				<pre>{{ periodSortOptions }}</pre>
+			</div>
 		</div>
 	</div>
 </template>
