1cadf3df02a4bc3376c18140a78aef7bc63894ed
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-07-29 00:12:44 -0500

    tiler tran home

--- app/components/tiller/BaseCards.vue
+++ app/components/tiller/BaseCards.vue
@@ -15,67 +15,123 @@ const props = defineProps<{
 	isLoading: boolean;
 }>();
 
-const { data: tillerStats } = useAsyncData<ITillerPageCardStat[]>(
-	'tillerstats',
-	async () => {
-		const counterAccCard: ITillerPageCardStat = {
-			title: 'Accounts',
-			icon: 'i-lucide-notebook-tabs',
-			value: props.homeTillerData.tillerAccounts.length,
-			counterValue: props.homeTillerData.tillerAccounts.length,
-			counterPrefix: 'Total Accounts: ',
-			to: '/dash/tiller/accounts'
-		};
-		const counterCatCard: ITillerPageCardStat = {
-			title: 'Categories',
-			icon: 'i-lucide-list',
-			value: props.homeTillerData.tillerCategories.length,
-			counterValue: props.homeTillerData.tillerCategories.length,
-			counterPrefix: 'Total Categories: '
-		};
-		// const catCard: ITillerPageCardStat = {
-		// 	title: 'Categories',
-		// 	icon: 'i-lucide-list',
-		// 	value: props.homeTillerData.tillerCategories.length
-		// };
+const tillerStatsComputed = computed<ITillerPageCardStat[]>(() => {
+	const counterAccCard: ITillerPageCardStat = {
+		title: 'Accounts',
+		icon: 'i-lucide-notebook-tabs',
+		value: props.homeTillerData.tillerAccounts.length,
+		counterValue: props.homeTillerData.tillerAccounts.length,
+		counterPrefix: 'Total Accounts: ',
+		to: '/dash/tiller/accounts'
+	};
+	const counterCatCard: ITillerPageCardStat = {
+		title: 'Categories',
+		icon: 'i-lucide-list',
+		value: props.homeTillerData.tillerCategories.length,
+		counterValue: props.homeTillerData.tillerCategories.length,
+		counterPrefix: 'Total Categories: '
+	};
 
-		// const accCard: ITillerPageCardStat = {
-		// 	title: 'Accounts',
-		// 	icon: 'i-lucide-notebook-tabs',
-		// 	value: props.homeTillerData.tillerAccounts.length
-		// };
+	// const totalTransCard: ITillerPageCardStat = {
+	// 	title: 'Tiller Trans',
+	// 	icon: 'i-lucide-banknote-x',
+	// 	value: props.homeTillerData.tillerTrans.length,
+	// 	counterValue: props.homeTillerData.tillerTrans.length,
+	// 	counterPrefix: 'Total Trans: '
+	// };
 
-		const totalTransCard: ITillerPageCardStat = {
-			title: 'Tiller Trans',
-			icon: 'i-lucide-banknote-x',
-			value: props.homeTillerData.tillerTrans.length,
-			counterValue: props.homeTillerData.tillerTrans.length,
-			counterPrefix: 'Total Trans: '
-		};
+	const unRecTrans = props.homeTillerData.tillerTrans.filter(
+		(t) => !t.generatedJournal && !t.reconciled && !t.excluded
+	);
 
-		const unRecTrans = props.homeTillerData.tillerTrans.filter((t) => !t.generatedJournal && !t.reconciled && !t.excluded);
+	const unRecTransCard: ITillerPageCardStat = {
+		title: 'Unreconciled Tiller Trans',
+		icon: 'i-lucide-circle-ellipsis',
+		value: unRecTrans.length,
+		counterValue: unRecTrans.length,
+		counterPrefix: 'Unreconciled Count:  '
+	};
 
-		const unRecTransCard: ITillerPageCardStat = {
-			title: 'Unreconciled Tiller Trans',
-			icon: 'i-lucide-circle-ellipsis',
-			value: unRecTrans.length,
-			counterValue: unRecTrans.length,
-			counterPrefix: 'Unreconciled Count:  '
-		};
+	const maxDateObj = props.homeTillerData.tillerTrans.reduce((max, current) => {
+		return current.date > max.date ? current : max;
+	});
 
-		return [counterAccCard, counterCatCard, totalTransCard, unRecTransCard];
-	},
-	{
-		watch: [() => props.homeTillerData],
-		default: () => []
-	}
-);
+	const maxDateAdded = Math.max(...props.homeTillerData.tillerTrans.map((t) => t.dateAdded.getTime()));
+
+	const tranDateCard: ITillerPageCardStat = {
+		title: `Created Trans Through ${formatUSDate(maxDateObj.date)} last on ${formatUSDate(new Date(maxDateAdded))}`,
+		icon: 'i-lucide-calendar-fold',
+		value: props.homeTillerData.tillerTrans.length,
+		counterValue: props.homeTillerData.tillerTrans.length,
+		counterPrefix: 'Total Trans: '
+	};
+
+	return [counterAccCard, counterCatCard, unRecTransCard, tranDateCard];
+});
+
+// const { data: tillerStats } = useAsyncData<ITillerPageCardStat[]>(
+// 	'tillerstats',
+// 	async () => {
+// 		const counterAccCard: ITillerPageCardStat = {
+// 			title: 'Accounts',
+// 			icon: 'i-lucide-notebook-tabs',
+// 			value: props.homeTillerData.tillerAccounts.length,
+// 			counterValue: props.homeTillerData.tillerAccounts.length,
+// 			counterPrefix: 'Total Accounts: ',
+// 			to: '/dash/tiller/accounts'
+// 		};
+// 		const counterCatCard: ITillerPageCardStat = {
+// 			title: 'Categories',
+// 			icon: 'i-lucide-list',
+// 			value: props.homeTillerData.tillerCategories.length,
+// 			counterValue: props.homeTillerData.tillerCategories.length,
+// 			counterPrefix: 'Total Categories: '
+// 		};
+// 		// const catCard: ITillerPageCardStat = {
+// 		// 	title: 'Categories',
+// 		// 	icon: 'i-lucide-list',
+// 		// 	value: props.homeTillerData.tillerCategories.length
+// 		// };
+
+// 		// const accCard: ITillerPageCardStat = {
+// 		// 	title: 'Accounts',
+// 		// 	icon: 'i-lucide-notebook-tabs',
+// 		// 	value: props.homeTillerData.tillerAccounts.length
+// 		// };
+
+// 		const totalTransCard: ITillerPageCardStat = {
+// 			title: 'Tiller Trans',
+// 			icon: 'i-lucide-banknote-x',
+// 			value: props.homeTillerData.tillerTrans.length,
+// 			counterValue: props.homeTillerData.tillerTrans.length,
+// 			counterPrefix: 'Total Trans: '
+// 		};
+
+// 		const unRecTrans = props.homeTillerData.tillerTrans.filter((t) => !t.generatedJournal && !t.reconciled && !t.excluded);
+
+// 		const unRecTransCard: ITillerPageCardStat = {
+// 			title: 'Unreconciled Tiller Trans',
+// 			icon: 'i-lucide-circle-ellipsis',
+// 			value: unRecTrans.length,
+// 			counterValue: unRecTrans.length,
+// 			counterPrefix: 'Unreconciled Count:  '
+// 		};
+
+// 		return [counterAccCard, counterCatCard, totalTransCard, unRecTransCard];
+// 	},
+// 	{
+// 		watch: [() => props.homeTillerData],
+// 		default: () => []
+// 	}
+// );
 </script>
 
 <template>
 	<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6">
+		<DisplaySpinner v-if="isLoading" />
 		<UPageCard
-			v-for="(stat, index) in tillerStats"
+			v-for="(stat, index) in tillerStatsComputed"
+			v-else
 			:key="index"
 			:icon="stat.icon"
 			:title="stat.title"
@@ -92,13 +148,17 @@ const { data: tillerStats } = useAsyncData<ITillerPageCardStat[]>(
 		>
 			<div class="flex items-center gap-2">
 				<DisplayCountto
-					v-if="stat.counterValue || stat.counterValue===0"
+					v-if="stat.counterValue || stat.counterValue === 0"
 					class="text-2xl font-semibold text-highlighted"
 					:end-value="stat.counterValue"
 					:prefix="stat.counterPrefix"
 					:suffix="stat.counterSuffix"
+					:duration="3000"
 				/>
-				<span v-else class="text-2xl font-semibold text-highlighted">
+				<span
+					v-else
+					class="text-2xl font-semibold text-highlighted"
+				>
 					{{ stat.value }}
 				</span>
 			</div>
