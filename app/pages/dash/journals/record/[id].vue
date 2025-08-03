ae51b421f3aaf1692b5f9f41673e6a50b900f307
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-08-03 00:22:37 -0500

    working on id

--- app/pages/dash/journals/record/[id].vue
+++ app/pages/dash/journals/record/[id].vue
@@ -15,11 +15,97 @@ const {
 		id: Array.isArray(route.params.id) ? route.params.id[0] : route.params.id
 	}
 });
+
+const mainDescription = computed<string>(() => {
+	if (tranData.value) {
+		return `View Detail for Tran ${tranData.value.id}`;
+	} else {
+		return `Tran Detail is Null`;
+	}
+});
+
+const periodDescription = computed<string>(() => {
+	if (tranData.value) {
+		return `View Detail for Tran in Period ${tranData.value.postingPeriod.label}`;
+	} else {
+		return `Tran Detail is Null`;
+	}
+});
+
+const infoDescription = computed<string>(() => {
+	if (tranData.value) {
+		return `View Detail for Tran #${tranData.value.tranNumber}`;
+	} else {
+		return `Tran Detail is Null`;
+	}
+});
 </script>
 
 <template>
 	<div>
-		{{ $route.params.id }}
+		<DisplaySpinner v-if="!tranData || pending" />
+		<UPageGrid v-else class="grid-cols-3 gap-4">
+			<UPageCard title="Main" :description="mainDescription">
+				<div class="grid grid-cols-2">
+					<div class="place-self-start font-bold">
+						Tran Number
+					</div>
+					<div class="place-self-end">
+						{{ tranData.tranNumber }}
+					</div>
+					<div class="place-self-start font-bold">
+						Tran Date
+					</div>
+					<div class="place-self-end">
+						{{ formatUSDate(tranData.tranDate) }}
+					</div>
+					<div class="place-self-start font-bold">
+						Tran ID
+					</div>
+					<div class="place-self-end">
+						{{ tranData.id }}
+					</div>
+				</div>
+			</UPageCard>
+			<UPageCard title="Period" :description="periodDescription">
+				<div class="grid grid-cols-2">
+					<div class="place-self-start font-bold">
+						Posting Period
+					</div>
+					<div class="justify-self-end">
+						{{ tranData.postingPeriod.label }}
+					</div>
+					<div class="place-self-start font-bold">
+						Period Locked
+					</div>
+					<div class="justify-self-end">
+						<DisplayBoolBadge :val="tranData.postingPeriod.locked" />
+					</div>
+					<div class="place-self-start font-bold">
+						Posting Period ID
+					</div>
+					<div class="justify-self-end">
+						{{ tranData.postingPeriod.id }}
+					</div>
+				</div>
+			</UPageCard>
+			<UPageCard title="Info" :description="infoDescription">
+				<div class="grid grid-cols-2">
+					<div class="place-self-start font-bold">
+						Test
+					</div>
+					<div class="place-self-end">
+						Test val
+					</div>
+					<div class="place-self-start font-bold">
+						Tran Source
+					</div>
+					<div class="justify-self-end">
+						{{ tranData.tranSource }}
+					</div>
+				</div>
+			</UPageCard>
+		</UPageGrid>
 		<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
 			<div>
 				tranData:
