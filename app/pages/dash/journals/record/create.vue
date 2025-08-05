103a337b0adc9756d1baea7ba54180588b6f142d
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-08-04 21:54:22 -0500

    period check create journal

--- app/pages/dash/journals/record/create.vue
+++ app/pages/dash/journals/record/create.vue
@@ -13,6 +13,9 @@ const { data: ledgerData, pending: ledgerDataPending } = await queryLedgerInputs
 		<div>
 			<h1>create journal</h1>
 			<div>status: {{ String(ledgerDataPending) }}</div>
+			<div v-if="ledgerData&& ledgerData.periods">
+				ledgerData.periods[0]: {{ String(typeof ledgerData.periods[0]?.startDate) }}
+			</div>
 		</div>
 
 		<UPageGrid v-if="ledgerData" class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
