e0a5664820962ddd8799aec9bb05ae26e589ba4a
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-08-09 03:08:08 -0500

    import base setup

--- app/pages/dash/journals/imports.vue
+++ app/pages/dash/journals/imports.vue
@@ -1,21 +1,49 @@
 <script lang="ts" setup>
+import type { StepperItem } from '@nuxt/ui';
 
+const items: StepperItem[] = [
+	{
+		title: 'File Upload',
+		// description: 'Choose your file here',
+		icon: 'i-lucide-file-up',
+		disabled: true,
+		value: 0
+	},
+	{
+		title: 'Field Mapping',
+		// description: 'Set your preferred shipping method',
+		// icon: 'i-lucide-truck',
+		disabled: true,
+		value: 1
+	},
+	{
+		title: 'Preview',
+		// description: 'Confirm your order',
+		disabled: true,
+		value: 2
+	},
+	{
+		title: 'Process',
+		// description: 'Confirm your order',
+		disabled: true,
+		value: 3
+	}
+];
+
+const activeStep = ref(0);
 </script>
 
 <template>
-	<div class="card flex justify-center">
-		<PStepper value="1" class="basis-[50rem]">
-			<PStepList>
-				<PStep value="1">
-					Header I
-				</PStep>
-				<PStep value="2">
-					Header II
-				</PStep>
-				<PStep value="3">
-					Header III
-				</PStep>
-			</PStepList>
-		</PStepper>
-	</div>
+	<UStepper
+		ref="stepper"
+		v-model="activeStep"
+		:items="items"
+		class="w-full"
+	>
+		<template #content="{ item }">
+			<div class="aspect-video">
+				This is the {{ item?.title }} step.
+			</div>
+		</template>
+	</UStepper>
 </template>
