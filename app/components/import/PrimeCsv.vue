2006546225ebc72d124d0e8b312ed4f909537365
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2025-08-09 18:08:01 -0500

    check in progress

--- app/components/import/PrimeCsv.vue
+++ app/components/import/PrimeCsv.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { FileUploadSelectEvent, FileUploadUploaderEvent } from 'primevue/fileupload';
+
 interface Props {
 	cardTitle: string;
 	isParentDisabled: boolean;
@@ -11,12 +13,47 @@ const props = withDefaults(defineProps<Props>(), {
 	multipleFiles: false
 });
 
+const uploadFile = defineModel<File>('uploadFile');
+
 const cardDescription = computed(() => `Upload data for ${props.cardTitle}`);
+
+const fileSelected = (event: FileUploadSelectEvent) => {
+	console.log(`fileSelected emit ${event.files}`);
+};
+
+const runUploader = (event: FileUploadUploaderEvent) => {
+	console.log(`runUploader emit ${event.files}`);
+
+	if (Array.isArray(event.files)) {
+		console.log(`files is array`);
+		uploadFile.value = event.files[0];
+	} else {
+		console.log(`files is not array`);
+		uploadFile.value = event.files;
+	}
+};
+
+const clearFileModel = () => {
+	console.log(`clearFileModel`);
+	uploadFile.value = undefined;
+};
 </script>
 
 <template>
 	<UPageCard :title="cardTitle" :description="cardDescription">
-		<PFileUpload :accept="acceptFiles" :multiple="multipleFiles">
+		<PFileUpload
+			:accept="acceptFiles"
+			:multiple="multipleFiles"
+			:file-limit="1"
+			auto
+			custom-upload
+			:disabled="isParentDisabled"
+			@select="fileSelected"
+			@uploader="runUploader"
+			@clear="clearFileModel"
+			@remove="clearFileModel"
+			@remove-uploaded-file="clearFileModel"
+		>
 			<template #empty>
 				<span>Drag and drop files to here to upload.</span>
 			</template>
--- app/pages/dash/journals/prime-imports.vue
+++ app/pages/dash/journals/prime-imports.vue
@@ -1,21 +1,112 @@
 <script lang="ts" setup>
+import type { StepperItem } from '@nuxt/ui';
 
+const isLoading = ref(false);
+
+const uploadedFile = ref<File>();
+
+const stepper = useTemplateRef('stepper');
+
+const items: StepperItem[] = [
+	{
+		title: 'File Upload',
+		// description: 'Choose your file here',
+		icon: 'i-lucide-file-up',
+		disabled: true,
+		value: 0,
+		slot: 'fileupload' as const
+	},
+	{
+		title: 'Field Mapping',
+		// description: 'Set your preferred shipping method',
+		// icon: 'i-lucide-truck',
+		disabled: true,
+		value: 1
+	},
+	{
+		title: 'Preview',
+		// description: 'Confirm your order',
+		disabled: true,
+		value: 2
+	},
+	{
+		title: 'Process',
+		// description: 'Confirm your order',
+		disabled: true,
+		value: 3
+	}
+];
+
+const activeStep = ref(0);
+
+const canAdvanceStepZero = computed<string>(() => {
+	if (isLoading.value) {
+		return `Page is Loading`;
+	} else if (!uploadedFile.value) {
+		return `Must upload file to proceed`;
+	} else {
+		return `Next Step`;
+	}
+});
+
+const advanceToMapping = async () => {
+	isLoading.value = true;
+	await new Promise((r) => setTimeout(r, 2000));
+	isLoading.value = false;
+};
 </script>
 
 <template>
-	<div class="card flex justify-center">
-		<PStepper value="1" class="basis-[50rem]">
-			<PStepList>
-				<PStep value="1">
-					Header I
-				</PStep>
-				<PStep value="2">
-					Header II
-				</PStep>
-				<PStep value="3">
-					Header III
-				</PStep>
-			</PStepList>
-		</PStepper>
+	<div class="w-full">
+		<UStepper
+			ref="stepper"
+			v-model="activeStep"
+			:items="items"
+			class="w-full"
+		>
+			<template #fileupload>
+				<ImportPrimeCsv
+					v-model:upload-file="uploadedFile"
+					card-title="Choose Journal File"
+					:is-parent-disabled="isLoading"
+				/>
+			</template>
+		</UStepper>
+		<div v-if="activeStep===0">
+			<UTooltip :text="canAdvanceStepZero">
+				<UButton
+					label="Next"
+					trailing-icon="i-lucide-arrow-right"
+					:disabled="canAdvanceStepZero !=='Next Step' || isLoading ||!uploadedFile"
+					:loading="isLoading"
+					class="w-1/4 place-items-center justify-center text-center"
+					@click="advanceToMapping"
+				/>
+			</UTooltip>
+		</div>
+		<div class="flex gap-2 justify-between mt-4">
+			<UButton
+				v-if="activeStep>0"
+				leading-icon="i-lucide-arrow-left"
+				:disabled="!stepper?.hasPrev ||isLoading||!uploadedFile"
+				:loading="isLoading"
+				@click="stepper?.prev()"
+			>
+				Prev
+			</UButton>
+
+			<UButton
+				v-if="activeStep>0"
+				trailing-icon="i-lucide-arrow-right"
+				:disabled="!stepper?.hasNext||isLoading"
+				:loading="isLoading"
+				@click="stepper?.next()"
+			>
+				Next
+			</UButton>
+		</div>
+		<div v-if="uploadedFile">
+			<h2>{{ uploadedFile.name }}</h2>
+		</div>
 	</div>
 </template>
